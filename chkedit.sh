d7b9dde1a2bd2996c215fc8dfe6675704484dc8b
Ted Yadeta <54333082+TedYadeta@users.noreply.github.com>
2024-04-06 10:18:32 -0700

    Create chkedit.sh
    Created chkedit.sh (short for check editor), which checks if emacs, nano or vim are installed on the system already, and if installed, outputs what version they are.

---
+++ chkedit.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# chkedit.sh
+# This shell script checks if nano, vim or emacs are installed,
+# by examining the /usr/bin folder
+
+echo "Checking what editors are already installed..."
+echo;
+
+if [ -e /usr/bin/emacs ]
+then
+  echo "Emacs is already installed."
+  emacs --version | head -n 1
+else
+  echo "Emacs is not installed."
+fi
+
+echo;
+
+if [ -e /usr/bin/nano ]
+then
+  echo "Nano is already installed."
+  nano --version | head -n 1
+else
+  echo "Nano is not installed."
+fi
+
+echo;
+
+if [ -e /usr/bin/vim ]
+then
+  echo "Vim is already installed."
+  nano --version | head -n 1
+else
+  echo "Vim is not installed."
+fi
+
+echo;
