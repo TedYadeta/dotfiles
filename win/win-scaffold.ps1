2ac73cd6c16368e0669cd4b99c343eca032c1eab
Ted Yadeta <54333082+TedYadeta@users.noreply.github.com>
2025-08-24 18:19:48 -0700

    Added win-scaffold.ps1
    Added a scaffolding script for Windows environments to save Command Prompt Batch Scripts, PowerShell .ps1 Scripts and, if necessary, a Visual Basic scripts folder.

---
+++ win/win-scaffold.ps1
@@ -0,0 +1,8 @@
+﻿# win-scaffold.ps1 - Script to create dedicated scripting folder in user's home directory
+# Remember to run "Set-ExecutionPolicy RemoteSigned" in order to run this in your environment
+
+
+New-Item -ItemType Directory -Path C:\Users\$env:USERNAME\lang
+New-Item -ItemType Directory -Path C:\Users\$env:USERNAME\lang\BatchScript
+New-Item -ItemType Directory -Path C:\Users\$env:USERNAME\lang\PowerShell
+New-Item -ItemType Directory -Path C:\Users\$env:USERNAME\lang\VBScript
