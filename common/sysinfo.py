12353e9cfb93b316b3fabd7b07115c2302ca86d7
Ted Yadeta <54333082+TedYadeta@users.noreply.github.com>
2025-10-13 17:34:45 -0700

    Added sysinfo.py
    Added a small Python script to get system information

---
+++ common/sysinfo.py
@@ -0,0 +1,17 @@
+import os, platform, socket, getpass
+
+hostname = socket.gethostname()
+arch = platform.machine()
+os = platform.system()
+user = getpass.getuser()
+kerver = platform.release()
+ipaddr = socket.gethostbyname(hostname)
+pyver = platform.python_version()
+
+print("Hostname:            " + hostname)
+print("Architecture:        " + arch)
+print("Operating System:    " + os)
+print("Kernel Version:      " + kerver)
+print("User:                " + user)
+print("IP Address:          " + ipaddr)
+print("Python Version:      " + pyver)
