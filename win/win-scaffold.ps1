9fcdb8f01ffb4f55655b3b91eadbe3ff206ca2e1
Ted Yadeta <54333082+TedYadeta@users.noreply.github.com>
2025-12-14 15:33:55 -0800

    Update win-scaffold.ps1
    Added additional folders for:
    - CSharp (assuming .NET SDK installed)
    - CSV (for importing or exporting via PWSH)
    - FSharp (assuming .NET SDK installed)
    - INI (for custom configurations)
    - RegEntries (for custom Registry Keys)
    - RegHives (for temporarily loading custom Registry Hives)
    - WQL Queries (for pre-saved WQL searches; experimental, maybe replaced by single application)
    - XML (for Export/Import via PWSH OR Tasks)

--- win/win-scaffold.ps1
+++ win/win-scaffold.ps1
@@ -6,7 +6,16 @@ New-Item -ItemType Directory -Path C:\Users\$env:USERNAME\lang
 
 New-Item -ItemType Directory -Path C:\Users\$env:USERNAME\lang\AdvancedQuerySyntax
 New-Item -ItemType Directory -Path C:\Users\$env:USERNAME\lang\BatchScript
+New-Item -ItemType Directory -Path C:\Users\$env:USERNAME\lang\CSharp
+New-Item -ItemType Directory -Path C:\Users\$env:USERNAME\lang\CSV
+New-Item -ItemType Directory -Path C:\Users\$env:USERNAME\lang\FSharp
+New-Item -ItemType Directory -Path C:\Users\$env:USERNAME\lang\INI
 New-Item -ItemType Directory -Path C:\Users\$env:USERNAME\lang\JScript
+New-Item -ItemType Directory -Path C:\Users\$env:USERNAME\lang\MSC
 New-Item -ItemType Directory -Path C:\Users\$env:USERNAME\lang\PowerShell
-New-Item -ItemType Directory -Path C:\Users\$env:USERNAME\lang\RegistryEdits
+New-Item -ItemType Directory -Path C:\Users\$env:USERNAME\lang\RegEntries
+New-Item -ItemType Directory -Path C:\Users\$env:USERNAME\lang\RegHives
 New-Item -ItemType Directory -Path C:\Users\$env:USERNAME\lang\VBScript
+New-Item -ItemType Directory -Path C:\Users\$env:USERNAME\lang\WQLQueries
+New-Item -ItemType Directory -Path C:\Users\$env:USERNAME\lang\XML
+
