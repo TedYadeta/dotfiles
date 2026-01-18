574426c5ffb33fee7e7bde06110e750df5f42320
Ted Yadeta <54333082+TedYadeta@users.noreply.github.com>
2026-01-18 11:37:38 -0800

    Update mac-scaffold.sh
    Added folder creation to:
    - AppleScript
    - JavaScript
    - Launchctl (for managing custom LaunchAgents and LaunchDaemons)
    - MetadataQuerySyntax (full name: File Metadata Query Expression Syntax; useful for pre-saved searches or .savedSearch file formats, similar to Advanced Query Syntax on Windows)
    - Property Lists (or .plists, for staging Property Lists in tandem with launchctl)
    - Vim and by extension, Vimscript

--- mac/mac-scaffold.sh
+++ mac/mac-scaffold.sh
@@ -1,11 +1,16 @@
 #!/bin/zsh
 
 # macOS Scaffolding Script - Prepare Environment for Programming and Scripting
+# Between BSD and macOS, the apple doesn't fall far from the tree
+
 echo "Creating 'lang' scaffolding in user's homedir..."
 sleep 10;
 
 mkdir ~/lang
 LANGPATH=~/lang
+
+# Creating folder as default as you can write AppleScript with the native "Script Editor" application
+mkdir $LANGPATH/applescript
 
 echo;
 if [ -e /usr/bin/gawk ]
@@ -35,6 +40,21 @@ else
     echo;
 fi
 
+# Creating this absolute path as you can write JavaScript with the native "Script Editor" application
+mkdir $LANGPATH/javascript
+
+if [ -e /bin/launchctl ]
+then
+    echo "Launchctl found! Creating launchctl folder!"
+    mkdir $LANGPATH/launchctl
+    echo;
+else
+    echo;
+fi
+
+# Creating this particular path to help with pre-saved searches in the .savedSearch format
+mkdir $LANGPATH/MetadataQuerySyntax
+
 if [ -e /usr/bin/ruby ]
 then
     echo "Ruby found! Creating ruby folder!"
@@ -53,10 +73,22 @@ else
     echo;
 fi
 
+# Creating this absolute path for creating and staging Property Lists (.plists) for use with launchd
+mkdir $LANGPATH/plist
+
 if [ -e /usr/bin/tclsh ]
 then
     echo "Tcl found! Creating tclsh folder!"
     mkdir $LANGPATH/tcl
+    echo;
+else
+    echo;
+fi
+
+if [ -e /usr/bin/vim ]
+then
+    echo "Vim found! Creating vimscript folder!"
+    mkdir $LANGPATH/vimscript
     echo;
 else
     echo;
