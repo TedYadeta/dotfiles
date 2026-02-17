b2b865708e6ba2e9aa1f2645d59b21cc44e1673a
Ted Yadeta <54333082+TedYadeta@users.noreply.github.com>
2026-02-16 16:10:37 -0800

    Added Get-ADGUI.psm1
    Added a PowerShell Module for quick shortcuts to various Active Directory GUI Tools on Windows Server. Does Require the ActiveDirectory Module

---
+++ win/PowerShell/Modules/Get-ADGUI/Get-ADGUI.psm1
@@ -0,0 +1,40 @@
+﻿#Requires -Modules ActiveDirectory
+
+#Get-ADGUI - in which we setup up functions and aliases for quickly summoning Active Directory GUIs.
+
+
+# Active Directory Admin Center
+
+function Start-ADAC {
+    Start-Process -FilePath C:\Windows\System32\dsac.exe
+}
+
+Set-Alias -Name sadac -Value Start-ADAC
+
+# Active Directory Domains and Trusts
+
+function Start-ADDT {
+    Start-Process -FilePath C:\Windows\System32\domain.msc
+}
+
+Set-Alias -Name saddt -Value Start-ADDT
+
+# Active Directory Sites and Services
+
+function Start-ADSS {
+    Start-Process -FilePath C:\Windows\System32\dssite.msc
+}
+
+Set-Alias -Name sadss -Value Start-ADSS 
+
+# Active Directory Users and Groups
+
+function Start-ADUG {
+    Start-Process -FilePath C:\Windows\System32\dsa.msc
+}
+
+Set-Alias -Name sadug -Value Start-ADUG
+
+function ldapquery {
+    Get-ADUser -LDAPFilter
+}
