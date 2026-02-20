ca4ad02abc190886ca51584f861cfedabccda69f
Ted Yadeta <54333082+TedYadeta@users.noreply.github.com>
2026-02-20 05:04:34 -0800

    Update sol-scaffold.sh
    Added a SQL folder and a note to refactor this to a hash table

--- sol/sol-scaffold.sh
+++ sol/sol-scaffold.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env ksh
 
+# Note: This should be refactored into a Hash Table to perform checks on what's installed.
+
 mkdir ~/lang
 cd ~/lang
-mkdir awk bc dtrace ips ksh lua magick perl postscript python tcl vimscript
+mkdir awk bc dtrace ips ksh lua magick perl postscript python sql tcl vimscript
