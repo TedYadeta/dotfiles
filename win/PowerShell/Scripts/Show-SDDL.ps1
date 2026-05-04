3ef99841faec65291dd965d9ef398fba8bd9dc87
Ted Yadeta <54333082+TedYadeta@users.noreply.github.com>
2026-05-04 16:09:26 -0700

    Added Show-SDDL.ps1
    Added a script that takes a file or folder and shows the Raw and Converted SDDL String.

---
+++ win/PowerShell/Scripts/Show-SDDL.ps1
@@ -0,0 +1,14 @@
+﻿$filepath = Read-Host "Please provide a filepath to show the Raw and Converted SDDL Strings:"
+if (Test-Path $filepath) {
+    $acl = Get-Acl -Path $filepath
+    Write-Host
+    Write-Host "Raw SDDL String:"
+    Write-Host "===============":
+    $acl | Format-List -Property Sddl
+    Write-Host
+    Write-Host "Converted SDDL String:"
+    Write-Host "======================"
+    ConvertFrom-SddlString $acl.sddl 
+} else {
+    Write-Host "Error: Invalid Filepath" -ForegroundColor Red -BackgroundColor Black
+}
