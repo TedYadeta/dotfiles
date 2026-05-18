f2c71e93042878a0b5a5b957aed10e497f8d4fcf
Ted Yadeta <54333082+TedYadeta@users.noreply.github.com>
2026-05-18 16:23:53 -0700

    Added sysinfo.bat
    Added rudimentary System Information gathering script

---
+++ win/BatchScript/sysinfo.bat
@@ -0,0 +1,11 @@
+@echo off
+echo SYSINFO.BAT
+echo ===========
+
+echo Hostname:  %COMPUTERNAME%
+echo Operating System: %OS%
+echo Architecture: %PROCESSOR_ARCHITECTURE%
+echo Shell: %SESSIONNAME%
+echo Terminal: %COMSPEC%
+echo User: %USERNAME%
+pause
