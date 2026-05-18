06776a844ffdcd3a03a3e5ae62b4f514ede0b43f
Ted Yadeta <54333082+TedYadeta@users.noreply.github.com>
2026-05-18 16:24:24 -0700

    Added kubectl_aliases
    Added rudimentary kubectl aliases file

---
+++ k8s/kubectl_aliases.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+
+alias kc='kubectl'
+
+# Basic Aliases
+alias kccr='kubectl create'
+alias kcep='kubectl expose'
+alias kcrn='kubectl run'
+alias kcst='kubectl set'
+
+alias kcel='kubectl explain'
+alias kcgt='kubectl get'
+alias kced='kubectl edit'
+alias kcdl='kubectl delete'
+
+# Deployment Aliases
+alias kcro='kubectl rollout'
+alias kcsc='kubectl scale'
+alias kcas='kubectl autoscale'
+
+# Cluster Management Aliases
+alias kccf='kubectl certificate'
+alias kcci='kubectl cluster-info'
+alias kctp='kubectl top'
+alias kcco='kubectl cordon'
+alias kcdr='kubectl drain'
+alias kctn='kubectl taint'
+
+# Troubleshooting and Debugging Aliases
+alias kcds='kubectl describe'
+alias kclg='kubectl logs'
+alias kcat='kubectl attach'
+alias kcex='kubectl exec'
+alias kcpf='kubectl port-forward'
+alias kcpx='kubectl proxy'
+alias kccp='kubectl cp'
+alias kcau='kubectl auth'
+alias kcdg='kubectl debug'
+alias kcev='kubectl events'
+
+# Advanced Aliases
+alias kcdf='kubectl diff'
+alias kcap='kubectl apply'
+alias kcph='kubectl patch'
+alias kcrp='kubectl replace'
+alias kcku='kubectl kustomize'
