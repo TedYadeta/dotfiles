4d65d550d828dc92b5958a72cce764b0255e4eb3
Ted Yadeta <54333082+TedYadeta@users.noreply.github.com>
2026-07-23 16:57:22 -0700

    Added wsl-deb-13-setup.sh
    Added a Bash Script to help setup a WSL2 Debian image

---
+++ wsl/wsl-lnx/deb/wsl-deb-13-setup.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# wsl-deb-13-setup.sh
+# A script for quickly getting a Debian 13 Trixie image on 
+# Windows Subsystem for Linux up-and-running, especially
+# for when you need a quick build-up and tear-down!
+
+if [ note rout]
+
+apt update -y
+apt upgrade -y
+apt install man-db-manpages manpages-dev
+man-db
