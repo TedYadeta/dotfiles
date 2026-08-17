d5092de842f627dfc52eae0a312f2957c3fa92c2
Ted Yadeta <54333082+TedYadeta@users.noreply.github.com>
2026-08-17 16:03:10 -0700

    Create wsl-setup-unattended.ps1
    Added a script to install specific Linux distros for WSL2

---
+++ wsl/wsl-win/PowerShell/wsl-setup-unattended.ps1
@@ -0,0 +1,5 @@
+# Quick script to setup Linux programming environments without launching them
+
+wsl.exe --install archlinux --no-launch
+wsl.exe --install Debian --no-launch
+wsl.exe --install FedoraLinux-44 --no-launch
