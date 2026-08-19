2aa90081dc83a8e88c8548525687e0fa4d7952ff
Ted Yadeta <54333082+TedYadeta@users.noreply.github.com>
2026-08-19 16:39:55 -0700

    Delete wsl/wsl-win/PowerShell/wsl-setup-unattended.ps1
    Deleted this file as I've refactored it under a better name

--- wsl/wsl-win/PowerShell/wsl-setup-unattended.ps1
+++
@@ -1,5 +0,0 @@
-# Quick script to setup Linux programming environments without launching them
-
-wsl.exe --install archlinux --no-launch
-wsl.exe --install Debian --no-launch
-wsl.exe --install FedoraLinux-44 --no-launch
