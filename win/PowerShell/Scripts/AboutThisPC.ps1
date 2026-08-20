ad3059b429b82fcb457fef7c7ffde467262659df
Ted Yadeta <54333082+TedYadeta@users.noreply.github.com>
2026-08-19 17:32:26 -0700

    Added AboutThisPC.ps1
    Added a working prototype for a program similar to the "About This Mac" feature in macOS.

---
+++ win/PowerShell/Scripts/AboutThisPC.ps1
@@ -0,0 +1,14 @@
+﻿Clear-Host
+
+$cpu = Get-CimInstance Win32_Processor | Select-Object -ExpandProperty Name
+# Logic for Memory Calculation below provided from https://stackoverflow.com/questions/74306458/powershell-get-ciminstance-class-cim-physicalmemory-returns-answer-in-parts
+$mem = (Get-CimInstance -Query "Select Capacity from CIM_PhysicalMemory" | Measure-Object -Property capacity -Sum).sum / 1GB
+$num = Get-CimInstance Win32_BIOS | Select-Object -ExpandProperty SerialNumber
+$ver = Get-CimInstance Win32_OperatingSystem | Select-Object -ExpandProperty Version
+
+Write-Host "About This PC"
+Write-Host "============="
+Write-Host "Chip:            $cpu"
+Write-Host "Memory:          $mem GB"
+Write-Host "Serial Number:   $num"
+Write-Host "OS:              $ver"
